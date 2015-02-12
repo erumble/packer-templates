ce49cdeb3720c6bbb765350f2e9c2e20ae347a97
Eric Rumble <eric.rumble@sage.com>
2015-02-12 17:25:54 -0500

    install individual packages and check md5sum

--- scripts/centos/install_ruby.sh
+++ scripts/centos/install_ruby.sh
@@ -1,25 +1,37 @@
 #!/bin/bash -eux
 
 VER='2.2.0'
+MD5='cd03b28fd0b555970f5c4fd481700852'
 
-yum groupinstall -y "Development Tools"
-yum install -y openssl-devel
+# Install requisite packages
+yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel \
+  make bzip2 autoconf automake libtool bison iconv-devel
 
+# make a temporary directory to install from
 if [[ ! -d ~vagrant/ruby-$VER ]]; then
   mkdir -p ~vagrant/ruby-$VER
 fi
 
+# install ruby
 pushd ~vagrant/ruby-$VER
   wget http://cache.ruby-lang.org/pub/ruby/2.2/ruby-$VER.tar.gz
-  tar -xzf ruby-$VER.tar.gz
+
+  if [[ $(md5sum "ruby-$VER.tar.gz" != $MD5* ]]; then
+    echo "ruby-$VER.tar.gz has an invalid checksum. Aborting."
+    exit 1
+  fi
+
+  tar -zxvf ruby-$VER.tar.gz
   cd ruby-$VER
   ./configure prefix=/usr
   make
   make install
 popd
 
+# clean up after ourself
 rm -rf ~vagrant/ruby-$VER
 
+# install the bundler gem, it's useful
 if type -P gem >/dev/null 2>&1; then
   gem update --system
   gem install bundler
