078fef0de6e3935fc61f430a9c7b8bc57695ef80
Eric Rumble <eric.rumble@sage.com>
2015-10-12 10:37:57 -0400

    move install scripts to the vagrant repo

--- scripts/install-scripts/install_chefdk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eux
-
-mkdir -p /tmp/chef
-pushd /tmp/chef
-wget https://opscode-omnibus-packages.s3.amazonaws.com/el/7/x86_64/chefdk-0.6.2-1.el7.x86_64.rpm
-yum localinstall -y chefdk-0.6.2-1.el7.x86_64.rpm
-popd
-rm -rf /tmp/chef
--- scripts/install-scripts/install_mysql.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eux
-
-yum localinstall -y http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm
-yum install -y mysql-community-server mysql-community-devel
-service mysqld start
-chkconfig --levels 235 mysqld on
-
