606b63e0e82f9683712715ed3fbb08c9f6879d4e
Eric Rumble <eric.rumble@sage.com>
2017-03-28 18:36:53 -0400

    update scripts

--- scripts/build-scripts/cleanup.sh
+++ scripts/build-scripts/cleanup.sh
@@ -17,10 +17,42 @@ rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
 rm -rf /dev/.udev/;
 
 for ndev in `ls -1 /etc/sysconfig/network-scripts/ifcfg-*`; do
-    if [ "`basename $ndev`" != "ifcfg-lo" ]; then
-        sed -i '/^HWADDR/d' "$ndev";
-        sed -i '/^UUID/d' "$ndev";
-    fi
+  if [ "`basename $ndev`" != "ifcfg-lo" ]; then
+    sed -i '/^HWADDR/d' "$ndev";
+    sed -i '/^UUID/d' "$ndev";
+  fi
 done
+
+# new-style network device naming for centos7
+if grep -q -i "release 7" /etc/redhat-release ; then
+  # radio off & remove all interface configration
+  nmcli radio all off
+  /bin/systemctl stop NetworkManager.service
+  for ifcfg in `ls /etc/sysconfig/network-scripts/ifcfg-* |grep -v ifcfg-lo` ; do
+    rm -f $ifcfg
+  done
+  rm -rf /var/lib/NetworkManager/*
+
+  echo "==> Setup /etc/rc.d/rc.local for CentOS7"
+  cat <<-'EOF' > /etc/rc.d/rc.local
+		#BEGIN
+		LANG=C
+		# delete all connection
+		for con in \`nmcli -t -f uuid con\`; do
+		  if [ "\$con" != "" ]; then
+		    nmcli con del \$con
+		  fi
+		done
+		# add gateway interface connection.
+		gwdev=\`nmcli dev | grep ethernet | egrep -v 'unmanaged' | head -n 1 | awk '{print \$1}'\`
+		if [ "\$gwdev" != "" ]; then
+		  nmcli c add type eth ifname \$gwdev con-name \$gwdev
+		fi
+		sed -i "/^#BEGIN/,/^#END/d" /etc/rc.d/rc.local
+		chmod -x /etc/rc.d/rc.local
+		#END
+		EOF
+  chmod +x /etc/rc.d/rc.local
+fi
 
 rm -f VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?;
