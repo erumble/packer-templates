3e9a32bd22950759b34a95ac07b56dfc8c6d777b
Eric Rumble <eric.rumble@digitalenvoy.net>
2017-07-27 15:43:11 -0400

    migrate from atlas to vagrant cloud

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 Inspired by, and heavily borrowed from [Chef Bento Boxes](https://github.com/chef/bento)
 
 #### Built With
-* [Packer](https://www.packer.io/) v0.12.3
-* [Vagrant](https://www.vagrantup.com/) v1.9.3
-* [VirtualBox](https://www.virtualbox.org/) v5.1.18
+* [Packer](https://www.packer.io/)
+* [Vagrant](https://www.vagrantup.com/)
+* [VirtualBox](https://www.virtualbox.org/)
 
-#### Hosted on [Atlas](https://atlas.hashicorp.com/vagrant)
-* [CentOS 6](https://atlas.hashicorp.com/erumble/boxes/centos6-x64) `vagrant init erumble/centos6-x64; vagrant up`
-* [CentOS 7](https://atlas.hashicorp.com/erumble/boxes/centos7-x64) `vagrant init erumble/centos7-x64; vagrant up`
+#### Hosted on [Vagrant Cloud](https://app.vagrantup.com/)
+* [CentOS 6](https://app.vagrantup.com/erumble/boxes/centos6-x64) `vagrant init erumble/centos6-x64; vagrant up`
+* [CentOS 7](https://app.vagrantup.com/erumble/boxes/centos7-x64) `vagrant init erumble/centos7-x64; vagrant up`
+
+###### Refer to the descriptions on Vagrant Cloud for information regarding specific versions of the software used to build these boxes.
--- build.sh
+++ build.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
 
-# create a description of the box to use in Atlas and Github
+# create a description of the box to use in Vagrant Cloud
+vbox_version=$(VBoxManage --version | awk -F 'r' '{print $1}')
+
 vm_description="#### Built With
 * [Packer](https://www.packer.io/) v$(packer --version)
 * [Vagrant](https://www.vagrantup.com/) v$(vagrant --version | awk '{print $2}')
-* [VirtualBox](https://www.virtualbox.org/) v$(VBoxManage --version | awk -F 'r' '{print $1}')"
-
-# regen the README.md file, be sure to commit and push after a build
-cat <<EOF > README.md
-### Packer templates for CentOS 6 and 7
-
-Inspired by, and heavily borrowed from [Chef Bento Boxes](https://github.com/chef/bento)
-
-${vm_description}
-
-EOF
-
-# Notice the single quotes around the heredoc delimiter? backticks
-cat <<'EOF' >> README.md
-#### Hosted on [Atlas](https://atlas.hashicorp.com/vagrant)
-* [CentOS 6](https://atlas.hashicorp.com/erumble/boxes/centos6-x64) `vagrant init erumble/centos6-x64; vagrant up`
-* [CentOS 7](https://atlas.hashicorp.com/erumble/boxes/centos7-x64) `vagrant init erumble/centos7-x64; vagrant up`
-EOF
+* [VirtualBox](https://www.virtualbox.org/) v${vbox_version}"
 
 # do the build
 packer build \
   -var "description=${vm_description}" \
+  -var "version=${vbox_version}" \
   $1
-
-# rollback the README.md if the build fails
-if [[ $? != 0 ]]; then
-  git checkout -- README.md
-fi
-
